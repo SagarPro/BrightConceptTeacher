d4d3ac4c1ed9f785905ddd46c2453580676271e4
supradip-muralidharan <supradip.muralidharan@gmail.com>
2018-05-15 16:40:43 +0530

    Added Dynamo Gradle & Data Model

--- app/src/main/java/sagu/supro/BCT/tv/Start.java
+++ app/src/main/java/sagu/supro/BCT/tv/Start.java
@@ -10,12 +10,16 @@ import android.widget.Toast;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
 import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 import sagu.supro.BCT.R;
 
 public class Start extends Activity {
 
     public static final String TAG = Start.class.getName();
+    // Declare a DynamoDBMapper object
+    DynamoDBMapper dynamoDBMapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,21 @@ public class Start extends Activity {
                 Log.d(TAG, "AWSMobileClient is instantiated and you are connected to AWS!");
             }
         }).execute();
+
+        // Instantiate a AmazonDynamoDBMapperClient
+        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient(AWSMobileClient.getInstance().getCredentialsProvider());
+        this.dynamoDBMapper = DynamoDBMapper.builder()
+                .dynamoDBClient(dynamoDBClient)
+                .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                .build();
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                //DynamoDB calls go here
+            }
+        };
+        Thread mythread = new Thread(runnable);
+        mythread.start();
 
     }
 }
