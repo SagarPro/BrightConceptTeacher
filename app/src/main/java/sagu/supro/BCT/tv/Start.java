f8ed8ebbd002ed92ccb58d1b64b31326d45c0a7f
supradip-muralidharan <supradip.muralidharan@gmail.com>
2018-05-15 16:35:37 +0530

    removed Toasts

--- app/src/main/java/sagu/supro/BCT/tv/Start.java
+++ app/src/main/java/sagu/supro/BCT/tv/Start.java
@@ -36,8 +36,5 @@ public class Start extends Activity {
             }
         }).execute();
 
-        Toast.makeText(this, "Working", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Supro Commit 2", Toast.LENGTH_SHORT).show();
-
     }
 }
