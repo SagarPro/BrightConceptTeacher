4cf93f1436a9546f6b0afeb6dc7382d4ad2a4e26
supradip-muralidharan <supradip.muralidharan@gmail.com>
2018-05-16 10:22:59 +0530

    put in register

---
+++ app/src/main/java/sagu/supro/BCT/mobile/Register.java
@@ -0,0 +1,15 @@
+package sagu.supro.BCT.mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sagu.supro.BCT.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
