cbfea95f0f21be7835059e2cb1234bd0954a2da7
supradip-muralidharan <supradip.muralidharan@gmail.com>
2018-05-18 09:34:10 +0530

    UI of User 70% completed. Replaced Movie model with video. Implemented Dynamo DB with all tables

---
+++ app/src/main/java/sagu/supro/BCT/dynamo/NurseryVideosDO.java
@@ -0,0 +1,66 @@
+package sagu.supro.BCT.dynamo;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "bct-mobilehub-1215798483-Nursery_Videos")
+
+public class NurseryVideosDO {
+    private String _videoId;
+    private String _videoTitle;
+    private String _videoCardImg;
+    private String _videoDescription;
+    private String _videoUrl;
+
+    @DynamoDBHashKey(attributeName = "video_id")
+    @DynamoDBAttribute(attributeName = "video_id")
+    public String getVideoId() {
+        return _videoId;
+    }
+
+    public void setVideoId(final String _videoId) {
+        this._videoId = _videoId;
+    }
+    @DynamoDBRangeKey(attributeName = "video_title")
+    @DynamoDBAttribute(attributeName = "video_title")
+    public String getVideoTitle() {
+        return _videoTitle;
+    }
+
+    public void setVideoTitle(final String _videoTitle) {
+        this._videoTitle = _videoTitle;
+    }
+    @DynamoDBAttribute(attributeName = "video_card_img")
+    public String getVideoCardImg() {
+        return _videoCardImg;
+    }
+
+    public void setVideoCardImg(final String _videoCardImg) {
+        this._videoCardImg = _videoCardImg;
+    }
+    @DynamoDBAttribute(attributeName = "video_description")
+    public String getVideoDescription() {
+        return _videoDescription;
+    }
+
+    public void setVideoDescription(final String _videoDescription) {
+        this._videoDescription = _videoDescription;
+    }
+    @DynamoDBAttribute(attributeName = "video_url")
+    public String getVideoUrl() {
+        return _videoUrl;
+    }
+
+    public void setVideoUrl(final String _videoUrl) {
+        this._videoUrl = _videoUrl;
+    }
+
+}
